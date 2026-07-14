bc793f0955bc768d4f112f451926019b61e13fae
Sergei <sergei@email.com>
2026-07-14 15:47:41 +0500

    Updates again

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	//	port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
