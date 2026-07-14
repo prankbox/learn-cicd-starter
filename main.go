c2ece3333e77762f9062d4f12255bd0148cb62ee
Sergei <sergei@email.com>
2026-07-14 15:43:43 +0500

    Another try to trigger an error

--- main.go
+++ main.go
@@ -13,8 +13,6 @@ import (
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 
-	//	"github.com/bootdotdev/learn-cicd-starter/internal/database"
-
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
@@ -26,12 +24,12 @@ type apiConfig struct {
 var staticFiles embed.FS
 
 func main() {
-	err := godotenv.Load("env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("POT")
+	//	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
